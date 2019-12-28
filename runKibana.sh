52bd38fd3a0883769f589f5e839325e00739f366
Gorka Garcia <garcia.gorka@gmail.com>
2019-12-28 11:30:51 +0100

    docker: corrected container name for running kibana

--- runKibana.sh
+++ runKibana.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run --link `docker ps | awk ' /elastic-with-plugin/ { print $1 }'`:elasticsearch -p 5601:5601 kibana:7.4.2
+docker run --link `docker ps | awk ' /gotrovi-es/ { print $1 }'`:elasticsearch -p 5601:5601 kibana:7.4.2
