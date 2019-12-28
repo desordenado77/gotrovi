15c8d37ce111064d354cb27c0b8f92d800897baf
Gorka Garcia <garcia.gorka@gmail.com>
2019-12-28 11:53:13 +0100

    docker: use container in github packages

--- runElasticSearch.sh
+++ runElasticSearch.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" -v ${PWD}/es:/usr/share/elasticsearch/data gotrovi-es
+docker run -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" -v ${PWD}/es:/usr/share/elasticsearch/data docker.pkg.github.com/desordenado77/gotrovi-dockerfiles/gotrovi-es:7.4.2
